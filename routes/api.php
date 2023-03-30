79b2650413f099eb023f189fac969ed355d81052
BryanTite <bryandesafio@gmail.com>
2023-03-30 19:24:22 +0200

    modificaciones addPosts,Posts,editPost

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,14 @@ use App\Http\Controllers\API\UserController;
 
 Route::post('login',[UserController::class,'login']);
 Route::post('register',[UserController::class,'register']);
-Route::post('logout',[UserController::class,'logout'])->middleware(middleware:'auth:sanctum');
+Route::post('logout',[UserController::class,'logout'])->middleware(['auth:sanctum']);
+
+Route::group(['prefix' => 'posts', 'middleware' => 'auth:sanctum'], function(){
+    Route::get('/', [PostController::class, 'index']);
+    Route::post('add', [PostController::class, 'add']);
+
+});
+
 /*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
