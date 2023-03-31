4112d698469d9e1fcbf801c56e3b2ea9ff6656e0
BryanTite <bryandesafio@gmail.com>
2023-03-31 17:57:29 +0200

    Footer test

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -19,9 +19,94 @@
 <!-- contenido componentes -->
         <router-view></router-view>
 <!-- footer -->
+        <footer class="text-lg-start bg-light">
+
+            <!-- Section: Links  -->
+            <section>
+                <div class="footerColor">
+                    <!-- Grid row -->
+                    <div class="row tipografiaFooter">
+                        <!-- Grid column -->
+                        <div class= "col col-sm-2 pt-3 espacioLogo">
+                            <a href="producto/index"><img src="assets/images/img2/logo2.webp" alt="El logotipo de la página"></a>
+                        </div>
+
+                        <div class="col col-lg-3 pt-4 redesSocial">
+                            <!-- Content -->
+
+                            <h6 class="text-uppercase fw-bold mb-4">REDES Y LOCALIZACIÓN</h6>
+                            <p>
+                                Carrer Ntra. Sra. de Lourde, 34, 08750 Molins de Rei, Barcelona
+                            </p>
+                            <div class="alinearRedes">
+                                <div class="hoverRedes">
+                                    <a href="https://www.instagram.com/marisqueriasantos/?hl=es"><img src="assets/images/iconos/insta.svg" alt="Icono de Instagram"></a>
+                                </div>
+                                <div class="hoverRedes">
+                                    <a href="https://www.facebook.com/marisqueria.santos/"><img src="assets/images/iconos/fb.svg" alt="Icono de Facebook"></a>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col mb-4 col-sm-3 pt-4 iconosReserva">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                RESERVAS Y CONTACTO
+                            </h6>
+                            <p>
+                                <img src="assets/images/iconos/mail.svg" alt="Icono del Mail">
+                                seafoodreynas@gmail.com
+                            </p>
+                            <p>
+                                <img src="assets/images/iconos/movil.svg" alt="Icono del número de móvil">
+                                684759824
+                            </p>
+                            <p>
+                                <img src="assets/images/iconos/movil.svg" alt="Icono del númrero de teléfono fijo">
+                                936487258
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col mb-4 pt-4 politicas">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                PRIVACIDAD
+                            </h6>
+                            <p>
+                                <a href="producto/politicas" class="text-reset">Política de privacidad de datos</a>
+                            </p>
+                            <p>
+                                <a href="producto/cookies" class="text-reset">Cookies</a>
+                            </p>
+                            <p>
+                                <a href="producto/aviso" class="text-reset">Aviso legal</a>
+                            </p>
+                            <p>
+                                <a href="usuario/resena" class="text-reset">Reseña</a>
+                            </p>
+                        </div>
+
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </section>
+            <!-- Section: Links  -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3 copy" style="background-color: black; color: white;">
+                © 2023 Copyright:
+                <a class="text-reset fw-bold" href="producto/index">BikiniBottom.com</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
 
     </div>
-    
+
  </template>
  <script>
  export default {
@@ -52,10 +137,9 @@
                         console.error(error);
                     });
             })
- 
- 
+
+
         }
     },
  }
- </script> 
- 
+ </script>
