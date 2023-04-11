e8188c28bff3e0667f3f903f12f1f49348ac235f
dani-fdez-g <danielfernandezfp@ibf.cat>
2023-04-11 17:46:44 +0200

    estilos

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- header -->
     <div class="container-fluid">
-        <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
-            <a class="navbar-brand" href="#">Laravel Vue 3</a>
+        <nav class="navbar navbar-expand-sm navbar-light bg-info mb-4 titulos-navbar">
+            <a class="navbar-brand" href="#">Laravel Vue</a>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                     aria-expanded="false" aria-label="Toggle navigation"></button>
             <div class="navbar-nav" v-if="isLoggedin">
