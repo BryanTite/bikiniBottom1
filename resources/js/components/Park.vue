a3c1e8bec99dfa8010fafb84952eeb6ab1e9d4fb
dani-fdez-g <danielfernandezfp@ibf.cat>
2023-04-21 16:08:55 +0200

    instalacion del acuario diseño

---
+++ resources/js/components/Park.vue
@@ -0,0 +1,20 @@
+<template>
+<img src="../../assets/img/plano.jpg" alt="plano de las instalaciones del acuario">
+
+</template>
+
+  <script>
+  export default{
+      name: "Park"
+  }
+  </script>
+
+    <style scoped>
+.card-img-top{
+    border-radius: 0px!important;
+}
+
+.contenedor-info-grande{
+    display: inline-flex;
+}
+    </style>
