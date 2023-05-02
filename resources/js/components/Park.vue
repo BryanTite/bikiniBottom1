5653724a1c4fca309f0d011c9efb200988e2cb57
dani-fdez-g <danielfernandezfp@ibf.cat>
2023-05-02 18:37:07 +0200

    estilos y mapa interactivo

--- resources/js/components/Park.vue
+++ resources/js/components/Park.vue
@@ -1,10 +1,13 @@
 <template>
 <div class="mapa-container container">
-    <canvas id="gameCanvas" height="300" width="300">
-    <img class="mapa" style="float:left; margin-right:20px;" src="../../assets/img/plano.png" alt="plano de las instalaciones del acuario">
-</canvas>  <h2 class="subtitulos">EL PARQUE</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas consequuntur nesciunt autem, odit cumque commodi saepe similique deserunt. Sed corporis deleniti quas obcaecati quisquam laborum ad? Ex molestiae cum explicabo?</p>
-  
+    <canvas  @mousemove="onMouseMove" id="canvas" height="500" width="500" ref="canRef" style="float:left; margin-right:20px;"></canvas> 
+    <!-- <img id="mapa"  src="../../assets/img/plano.png" alt="plano de las instalaciones del acuario" class="d-none" ref="mapa"> -->
+<h2 class="subtitulos">EL PARQUE</h2>
+    <p>
+El parque acuático es un lugar fascinante que ofrece una experiencia única para los amantes de la vida marina. Con sus cuatro acuarios tropicales.
+
+El primer acuario se centra en los arrecifes de coral, donde los visitantes pueden ver una gran variedad de especies marinas coloridas y exóticas, incluyendo peces payaso, estrellas de mar y pulpos. El segundo acuario se enfoca en las zonas rocosas de la costa, con exhibiciones de animales como erizos de mar, cangrejos y langostas. El tercer acuario muestra las criaturas de las aguas tropicales profundas, donde se encuentran especies como el tiburón martillo, el pez espada y el calamar gigante. Y el cuarto y último acuario exhibe las especies que habitan en los ríos tropicales, como los peces de colores brillantes, las tortugas y las serpientes de agua.
+</p>
     <h2 class="subtitulos" >INSTALACIONES DE BINIKI BOTTOM </h2>
     <p>Un lugar privilegiado para conocer la <b class="morado">biodiversidad de los ecosistemas marinos</b>, en especial de las especies del Mediterráneo, y lanzar un mensaje sobre la necesidad e importancia de su conservación.   
     
@@ -25,11 +28,67 @@ En cuanto a la decoración y ambiente, muchos de estos restaurantes ofrecen un a
 
 </template>
 
-  <script>
-  export default{
-      name: "Park"
-  }
-  </script>
+
+
+<script>
+export default {
+  name: "Park",
+  data() {
+    return {
+      items: [
+        { id: 1, src: "img/plano1.webp" },
+        { id: 2, src: "img/plano2.webp" },
+        { id: 3, src: "img/plano4.webp" },
+        { id: 4, src: "img/plano3.webp" },
+      ],
+      currentImgIndex: 0,
+    };
+  },
+  mounted() {
+    this.canvas = this.$refs.canRef;
+    this.ctx = this.canvas.getContext("2d");
+    this.canvasWidth = this.canvas.width;
+    this.canvasHeight = this.canvas.height;
+    // Dividir el canvas en 4 partes iguales
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.canvasWidth / 2, 0);
+    this.ctx.lineTo(this.canvasWidth / 2, this.canvasHeight);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, this.canvasHeight / 2);
+    this.ctx.lineTo(this.canvasWidth, this.canvasHeight / 2);
+    this.ctx.stroke();
+  },
+  methods: {
+    onMouseMove(event) {
+      const x = event.offsetX;
+      const y = event.offsetY;
+      if (x < this.canvasWidth / 2 && y < this.canvasHeight / 2) {
+        // Parte superior izquierda
+        this.currentImgIndex = 1;
+      } else if (x >= this.canvasWidth / 2 && y < this.canvasHeight / 2) {
+        // Parte superior derecha
+        this.currentImgIndex = 2;
+      } else if (x < this.canvasWidth / 2 && y >= this.canvasHeight / 2) {
+        // Parte inferior izquierda
+        this.currentImgIndex = 3;
+      } else {
+        // Parte inferior derecha
+        this.currentImgIndex = 4;
+      }
+      this.drawImage();
+    },
+    drawImage() {
+      const img = new Image();
+      img.src = this.items[this.currentImgIndex - 1].src;
+      img.onload = () => {
+        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        this.ctx.drawImage(img, 0, 0, this.canvasWidth, this.canvasHeight);
+      };
+    },
+  },
+};
+</script>
 
     <style scoped>
 .mapa{
